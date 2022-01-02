03fb2509c010c3426bcb3602ab2c9a06c52a6fc9
ax3lt <43681465+ax3lt@users.noreply.github.com>
2022-01-02 20:18:46 +0100

    Added code for automating the update
    Now you can ./script.sh && java -jar server.jar without auto updating every time

--- updatePaper.sh
+++ updatePaper.sh
@@ -7,7 +7,22 @@ type="paper" # Supported variables actually: paper, travertine, waterfall, veloc
 version=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/ | jq -r .versions[-1]') # Latest version
 build=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/versions/$version/ | jq -r .builds[-1]') # Latest build
 download=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/versions/$version/builds/$build | jq -r .downloads.application.name') # Download name
-
 file="https://papermc.io/api/v2/projects/$type/versions/$version/builds/$build/downloads/$download" # Complete download string
 
-wget $file -O $name
+
+if [ ! -e latestBuildInstalled ]; then
+    touch latestBuildInstalled
+    echo "undefined" >> latestBuildInstalled
+    echo "undefined" >> latestBuildInstalled
+    echo "undefined" >> latestBuildInstalled
+fi
+
+
+if [ $(eval 'sed '1!d' latestBuildInstalled') == $type ] && [ $(eval 'sed '2!d' latestBuildInstalled') == $version ] && [ $(eval 'sed '3!d' latestBuildInstalled') == $build ]; then
+    echo "JAR up to date"
+else
+    wget $file -O $name
+    echo $type > latestBuildInstalled
+    echo $version >> latestBuildInstalled
+    echo $build >> latestBuildInstalled
+fi
