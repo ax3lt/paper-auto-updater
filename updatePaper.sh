a1ae254bbd08ccc42f0eee746ac343f3ab70579d
ax3lt <43681465+ax3lt@users.noreply.github.com>
2022-01-02 18:12:48 +0100

    Update updatePaper.sh

--- updatePaper.sh
+++ updatePaper.sh
@@ -3,7 +3,7 @@
 
 read -p "Output name: " name
 
-type="paper" # Supported varibles actually: paper, travertine, waterfall, velocity
+type="paper" # Supported variables actually: paper, travertine, waterfall, velocity
 version=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/ | jq -r .versions[-1]') # Latest version
 build=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/versions/$version/ | jq -r .builds[-1]') # Latest build
 download=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/versions/$version/builds/$build | jq -r .downloads.application.name') # Download name
