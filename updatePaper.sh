86c58a22bfe933d1210539dc8f49352b7943f471
ax3lt <43681465+ax3lt@users.noreply.github.com>
2022-01-02 18:10:23 +0100

    Update updatePaper.sh

--- updatePaper.sh
+++ updatePaper.sh
@@ -1,11 +1,13 @@
 # name = "server.jar"
+# version="1.18.1" # Custom static version
+
 read -p "Output name: " name
 
-#version="1.18.1" # Custom static version
-version=$(eval "curl -s https://papermc.io/api/v2/projects/paper/ | jq -r .versions[-1]") # Latest version
-build=$(eval 'curl -s https://papermc.io/api/v2/projects/paper/versions/$version/ | jq -r .builds[-1]') # Latest build
-download=$(eval 'curl -s https://papermc.io/api/v2/projects/paper/versions/$version/builds/$build | jq -r .downloads.application.name') # Download name
+type="paper"
+version=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/ | jq -r .versions[-1]') # Latest version
+build=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/versions/$version/ | jq -r .builds[-1]') # Latest build
+download=$(eval 'curl -s https://papermc.io/api/v2/projects/$type/versions/$version/builds/$build | jq -r .downloads.application.name') # Download name
 
-file="https://papermc.io/api/v2/projects/paper/versions/$version/builds/$build/downloads/$download" # Complete download string
+file="https://papermc.io/api/v2/projects/$type/versions/$version/builds/$build/downloads/$download" # Complete download string
 
 wget $file -O $name
